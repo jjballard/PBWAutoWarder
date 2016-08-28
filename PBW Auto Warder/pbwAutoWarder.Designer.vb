24af2dd0ce570059e2ef9e7863b9bd6062549e54
jjballard <karaksindru@gmail.com>
2016-08-27 19:02:24 -0700

    Initial Git commit

---
+++ PBW Auto Warder/pbwAutoWarder.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.butWard = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBoardID = New System.Windows.Forms.TextBox()
+        Me.txtCharID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBody = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lstPostIDs = New System.Windows.Forms.ListBox()
+        Me.btnGetPosts = New System.Windows.Forms.Button()
+        Me.cbxBoards = New System.Windows.Forms.ComboBox()
+        Me.cbxChars = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'butWard
+        '
+        Me.butWard.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.butWard.Location = New System.Drawing.Point(98, 226)
+        Me.butWard.Name = "butWard"
+        Me.butWard.Size = New System.Drawing.Size(75, 23)
+        Me.butWard.TabIndex = 0
+        Me.butWard.Text = "Ward"
+        Me.butWard.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Board ID"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'txtBoardID
+        '
+        Me.txtBoardID.Location = New System.Drawing.Point(67, 9)
+        Me.txtBoardID.Name = "txtBoardID"
+        Me.txtBoardID.Size = New System.Drawing.Size(158, 20)
+        Me.txtBoardID.TabIndex = 2
+        Me.txtBoardID.Text = "16412"
+        '
+        'txtCharID
+        '
+        Me.txtCharID.Location = New System.Drawing.Point(67, 35)
+        Me.txtCharID.Name = "txtCharID"
+        Me.txtCharID.Size = New System.Drawing.Size(205, 20)
+        Me.txtCharID.TabIndex = 4
+        Me.txtCharID.Text = "133861"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Char ID"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Post IDs"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'txtTitle
+        '
+        Me.txtTitle.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtTitle.Location = New System.Drawing.Point(67, 123)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(205, 20)
+        Me.txtTitle.TabIndex = 8
+        Me.txtTitle.Text = "Reaper Ward"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 123)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(27, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Title"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'txtBody
+        '
+        Me.txtBody.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtBody.Location = New System.Drawing.Point(67, 149)
+        Me.txtBody.Multiline = True
+        Me.txtBody.Name = "txtBody"
+        Me.txtBody.Size = New System.Drawing.Size(205, 71)
+        Me.txtBody.TabIndex = 10
+        Me.txtBody.Text = "Standard Warding"
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 149)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Body"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lstPostIDs
+        '
+        Me.lstPostIDs.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lstPostIDs.FormattingEnabled = True
+        Me.lstPostIDs.HorizontalScrollbar = True
+        Me.lstPostIDs.Items.AddRange(New Object() {"112649", "112687", "112720"})
+        Me.lstPostIDs.Location = New System.Drawing.Point(67, 61)
+        Me.lstPostIDs.Name = "lstPostIDs"
+        Me.lstPostIDs.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lstPostIDs.Size = New System.Drawing.Size(205, 56)
+        Me.lstPostIDs.TabIndex = 11
+        '
+        'btnGetPosts
+        '
+        Me.btnGetPosts.Location = New System.Drawing.Point(231, 9)
+        Me.btnGetPosts.Name = "btnGetPosts"
+        Me.btnGetPosts.Size = New System.Drawing.Size(41, 23)
+        Me.btnGetPosts.TabIndex = 12
+        Me.btnGetPosts.Text = "..."
+        Me.btnGetPosts.UseVisualStyleBackColor = True
+        '
+        'cbxBoards
+        '
+        Me.cbxBoards.FormattingEnabled = True
+        Me.cbxBoards.Location = New System.Drawing.Point(278, 8)
+        Me.cbxBoards.Name = "cbxBoards"
+        Me.cbxBoards.Size = New System.Drawing.Size(121, 21)
+        Me.cbxBoards.TabIndex = 14
+        '
+        'cbxChars
+        '
+        Me.cbxChars.FormattingEnabled = True
+        Me.cbxChars.Location = New System.Drawing.Point(278, 35)
+        Me.cbxChars.Name = "cbxChars"
+        Me.cbxChars.Size = New System.Drawing.Size(121, 21)
+        Me.cbxChars.TabIndex = 15
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 261)
+        Me.Controls.Add(Me.cbxChars)
+        Me.Controls.Add(Me.cbxBoards)
+        Me.Controls.Add(Me.btnGetPosts)
+        Me.Controls.Add(Me.lstPostIDs)
+        Me.Controls.Add(Me.txtBody)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCharID)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtBoardID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.butWard)
+        Me.Name = "Form1"
+        Me.Text = "Auto Warder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents butWard As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBoardID As TextBox
+    Friend WithEvents txtCharID As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBody As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lstPostIDs As ListBox
+    Friend WithEvents btnGetPosts As Button
+    Friend WithEvents cbxBoards As ComboBox
+    Friend WithEvents cbxChars As ComboBox
+End Class
---
+++ PBW Auto Warder/pbwAutoWarder.vb
@@ -0,0 +1,309 @@
+﻿Imports System.Net
+Imports System.Data.SQLite
+Imports System.Security.Cryptography
+
+Public Class Form1
+
+    Private PBWID As String
+    Private Chars As New Dictionary(Of String, String)
+    Private Boards As New Dictionary(Of String, String)
+    Private Posts As New Dictionary(Of String, String)
+
+    Private Sub butWard_Click(sender As Object, e As EventArgs) Handles butWard.Click
+
+        Dim postData As String = String.Empty
+        Dim postID As String = String.Empty
+
+        If lstPostIDs.SelectedItems.Count = 1 Then
+            postID = lstPostIDs.SelectedItem.ToString()
+            postData = replacePostData(postID)
+            SubmitForm(postData)
+
+        ElseIf lstPostIDs.SelectedItems.Count = 0 Then
+            For i = 0 To lstPostIDs.Items.Count - 1
+                postID = lstPostIDs.Items(i).ToString()
+                postData = replacePostData(postID)
+                SubmitForm(postData)
+            Next
+
+        ElseIf lstPostIDs.SelectedItems.Count > 1 Then
+            For i = 0 To lstPostIDs.SelectedItems.Count - 1
+                postID = lstPostIDs.SelectedItems(i).ToString()
+                postData = replacePostData(postID)
+                SubmitForm(postData)
+            Next
+
+        End If
+        RefreshPostIDs()
+
+    End Sub
+
+    Private Function replacePostData(postID As String) As String
+        Dim boardID As String = txtBoardID.Text
+        Dim charID As String = txtCharID.Text
+        Dim titleText As String = txtTitle.Text
+        Dim bodyText As String = txtBody.Text
+        Dim postData As String = String.Empty
+
+        Dim postDataGeneric As String =
+            "_b=BOARDID&_h=generic2.php&_p=POSTID&_n=99&_i=80&_g=44149&_e=er&_php=postreply.php&_title=TITLETEXT&_c=CHARID&_text=BODYTEXT&edit2request=" +
+            "Array%0D%0A%28%0D%0A++++%5B_b%5D+%3D%3E+BOARDID%0D%0A++++%5B_h%5D+%3D%3E+generic2.php%0D%0A++++%5B_p%5D+%3D%3E+POSTID%0D%0A++++%5B_n%5D+%3D%3E+" +
+            "99%0D%0A++++%5B_i%5D+%3D%3E+80%0D%0A++++%5B_g%5D+%3D%3E+44149%0D%0A++++%5B_e%5D+%3D%3E+er%0D%0A++++%5B_php%5D+%3D%3E+postreply.php%0D%0A++++%5B" +
+            "edit1request%5D+%3D%3E+Array%28++++%5B_b%5D+%3D%3E+BOARDID++++%5B_h%5D+%3D%3E+generic2.php++++%5B_p%5D+%3D%3E+113086++++%5B_n%5D+%3D%3E+99++++%5B_i%5D" +
+            "+%3D%3E+80++++%5B_g%5D+%3D%3E+44149++++%5B_e%5D+%3D%3E+er++++%5B_php%5D+%3D%3E+postreply.php++++%5BAC%5D+%3D%3E+3798559++++%5Barp_scroll_position%5D+" +
+            "%3D%3E+6607%29%0D%0A++++%5Bedit1url%5D+%3D%3E+%3CTD+NOWRAP%3E%3CA+TARGET%3D_blank+HREF%3Ddice1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26" +
+            "_i%3D80%26_g%3D44149%26_e%3Der%26_php%3Dpostreply.php%3E%3CBIG%3EDice%3C%2FBIG%3E%3C%2FA%3E%3CBR%3E%3CBR%3E%3CBIG%3EPlayers%3C%2FBIG%3E%3CBR%3E%A0%A0%A0%A0%3C" +
+            "INPUT+TYPE%3DRADIO+NAME%3D_c+VALUE%3D133861+%3E%3CA+TARGET%3D_blank+HREF%3Dchar_ed1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26_i%3D80%26_g" +
+            "%3D44149%26_e%3Der%26_php%3Dpostreply.php%26_c%3D133861%26_u%3D559%3ETalisman%3C%2FA%3E+%3CBR%3E%3CBIG%3ETogether%3C%2FBIG%3E%3CBR%3E%A0%A0%A0%A0%3CINPUT+TYPE%3DRADIO" +
+            "+NAME%3D_c+VALUE%3D133861+%3E%3CA+TARGET%3D_blank+HREF%3Dchar_ed1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26_i%3D80%26_g%3D44149%26" +
+            "_e%3Der%26_php%3Dpostreply.php%26_c%3D133861%26_u%3D559%3ETalisman%3C%2FA%3E+%3CBR%3E%3CBIG%3EPlayers%3C%2FBIG%3E%3CBR%3E%A0%A0%A0%A0%3C" +
+            "INPUT+TYPE%3DRADIO+NAME%3D_c+VALUE%3DCHARID+%3E%3CA+TARGET%3D_blank+HREF%3Dchar_ed1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26_i%3D80%26" +
+            "_g%3D44149%26_e%3Der%26_php%3Dpostreply.php%26_c%3DCHARID%26_u%3D559%3EJer+Stanton%3C%2FA%3E+%3CBR%3E%3CBIG%3EPrivate%3C%2FBIG%3E%3CBR%3E%A0%A0%A0%A0%3C" +
+            "INPUT+TYPE%3DRADIO+NAME%3D_c+VALUE%3DCHARID+%3E%3CA+TARGET%3D_blank+HREF%3Dchar_ed1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26_i%3D80%26" +
+            "_g%3D44149%26_e%3Der%26_php%3Dpostreply.php%26_c%3DCHARID%26_u%3D559%3EJer+Stanton%3C%2FA%3E+%3CBR%3E%3CBIG%3ETogether%3C%2FBIG%3E%3CBR%3E%A0%A0%A0%A0%3C" +
+            "INPUT+TYPE%3DRADIO+NAME%3D_c+VALUE%3DCHARID+%3E%3CA+TARGET%3D_blank+HREF%3Dchar_ed1.php%3F_b%3DBOARDID%26_h%3Dgeneric2.php%26_p%3D113086%26_n%3D99%26_i%3D80%26" +
+            "_g%3D44149%26_e%3Der%26_php%3Dpostreply.php%26_c%3DCHARID%26_u%3D559%3EJer+Stanton%3C%2FA%3E+%3CBR%3E%3C%2FTD%3E%0D%0A++++%5B_title%5D+%3D%3E+TITLETEXT" +
+            "%0D%0A++++%5Bbutton%5D+%3D%3E+Preview+%3E%0D%0A++++%5B_c%5D+%3D%3E+CHARID%0D%0A++++%5B_text%5D+%3D%3E+BODYTEXT%0D%0A++++%5BAC%5D+%3D%3E+3798559%0D%0A" +
+            "++++%5Barp_scroll_position%5D+%3D%3E+1200%0D%0A%29%0D%0A&button=Post+%3E"
+
+        postData = postDataGeneric
+        postData = postData.Replace("BOARDID", boardID)
+        postData = postData.Replace("CHARID", charID)
+        postData = postData.Replace("TITLETEXT", titleText)
+        postData = postData.Replace("BODYTEXT", bodyText)
+        postData = postData.Replace("POSTID", postID)
+        Return postData
+    End Function
+
+    Private Sub SubmitForm(postData As String)
+        Dim request As WebRequest = WebRequest.Create("http://www.playbyweb.com/postreply.php")
+
+        If Not PBWID = String.Empty Then
+            request.Headers.Add(HttpRequestHeader.Cookie, String.Format("AC={0}", PBWID))
+        Else
+            request.Headers.Add(HttpRequestHeader.Cookie, String.Format("AC=3798559"))
+        End If
+
+        request.Method = "POST"
+
+        Dim byteArray As Byte()
+        byteArray = System.Text.Encoding.UTF8.GetBytes(postData)
+        request.ContentType = "application/x-www-form-urlencoded"
+        request.ContentLength = byteArray.Length
+
+        Dim dataStream As System.IO.Stream = request.GetRequestStream()
+        dataStream.Write(byteArray, 0, byteArray.Length)
+        dataStream.Close()
+
+    End Sub
+
+    Private Sub btnGetPosts_Click(sender As Object, e As EventArgs) Handles btnGetPosts.Click
+        RefreshPostIDs()
+
+    End Sub
+
+    Private Sub RefreshPostIDs()
+        If Not txtBoardID.Text = "" Then
+            Dim boardURL As String = String.Format("http://www.playbyweb.com/generic2.php?_b={0}", txtBoardID.Text)
+            Dim request As WebRequest = WebRequest.Create(boardURL)
+            request.Method = "GET"
+            Dim dataStream As System.IO.Stream ' = request.GetRequestStream
+            Dim response As WebResponse
+            response = request.GetResponse()
+            Dim strPostId As String = ""
+
+
+            Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
+            dataStream = response.GetResponseStream()
+
+            Dim reader As New System.IO.StreamReader(dataStream)
+            Dim responseFromServer As String = ""
+            lstPostIDs.Items.Clear()
+
+            While Not reader.EndOfStream
+                responseFromServer = reader.ReadLine()
+                If responseFromServer.Contains(" i (Reap in") Then
+                    strPostId = responseFromServer.Substring(responseFromServer.IndexOf("_p="), 9).Substring(3, 6)
+                    'Console.WriteLine(strPostId)
+                    lstPostIDs.Items.Add(strPostId)
+
+                End If
+            End While
+
+            'Console.WriteLine(responseFromServer)
+            reader.Close()
+            response.Close()
+
+        End If
+    End Sub
+
+    Private Sub pbwAutoWarder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PBWID = ""
+        PBWID = getCookie_Chrome()
+        If PBWID = "" Then PBWID = getCookie_Firefox()
+        If Not PBWID = "" Then
+            ' getChars(PBWID)
+            getBoards(PBWID)
+        End If
+        If txtBoardID.TextLength > 0 Then RefreshPostIDs()
+
+    End Sub
+
+    Private Sub getBoards(PBWID As String)
+        Boards.Clear()
+        ' ignore board 12518 (Staff Room)
+        Dim URL As String = "http://www.playbyweb.com/index.php"
+        Dim request As WebRequest = WebRequest.Create(URL)
+        request.Method = "GET"
+        Dim dataStream As System.IO.Stream ' = request.GetRequestStream
+        Dim response As WebResponse = request.GetResponse()
+        Dim strBoardID As String = ""
+        Dim strBoardName As String = ""
+
+        If Not PBWID = String.Empty Then
+            request.Headers.Add(HttpRequestHeader.Cookie, String.Format("AC={0}", PBWID))
+        Else
+            request.Headers.Add(HttpRequestHeader.Cookie, String.Format("AC="))
+        End If
+
+        Console.WriteLine(CType(response, HttpWebResponse).StatusDescription)
+        dataStream = response.GetResponseStream()
+
+        Dim reader As New System.IO.StreamReader(dataStream)
+        Dim responseFromServer As String = ""
+        lstPostIDs.Items.Clear()
+
+        While Not reader.EndOfStream
+            responseFromServer = reader.ReadLine()
+            If responseFromServer.Contains("_b=") Then
+                strBoardID = responseFromServer.Substring(responseFromServer.IndexOf("_b="), 9).Substring(3, 5)
+
+
+
+            End If
+        End While
+
+        'Console.WriteLine(responseFromServer)
+        reader.Close()
+
+
+    End Sub
+
+    Private Sub getChars(pBWID As String)
+        Chars.Clear()
+    End Sub
+
+    Private Function getCookie_Firefox() As String
+        Dim strPath As String = String.Empty
+        Dim strDb As String = String.Empty
+        Dim strTemp As String = String.Empty
+        strPath = GetFirefoxCookiePath()
+        Dim strReturn As String = String.Empty
+
+        If strPath = String.Empty Then
+            Return String.Empty
+        End If
+        Try
+            strTemp = strPath + ".temp"
+            strDb = "Data Source=" + strTemp + ";pooling=false"
+            System.IO.File.Copy(strPath, strTemp, True)
+
+            Dim sqlcon As New SQLiteConnection(strDb)
+            Dim cmd As SQLiteCommand = sqlcon.CreateCommand()
+            cmd.CommandText = "SELECT value FROM moz_cookies WHERE host LIKE '%playbyweb.com%' AND name LIKE '%AC%';"
+            sqlcon.Open()
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader()
+            While reader.Read()
+                strReturn = reader.GetString(0)
+                If Not strReturn.Equals(String.Empty) Then
+                    Exit While
+                End If
+            End While
+            Return strReturn
+
+        Catch ex As Exception
+            Return String.Empty
+        End Try
+
+        Return strReturn
+    End Function
+
+    Private Function GetFirefoxCookiePath() As String
+        Dim path As String = ""
+        path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        path += "\Mozilla\Firefox\Profiles\"
+        Try
+            Dim dinfo As New System.IO.DirectoryInfo(path)
+            Dim dir As System.IO.DirectoryInfo() = dinfo.GetDirectories("*.default*")
+
+            If Not dir.Length = 1 Then
+                Return String.Empty
+            End If
+
+            path += dir(0).Name + "\cookies.sqlite"
+        Catch ex As Exception
+            Return String.Empty
+        End Try
+
+        If Not System.IO.File.Exists(path) Then
+            Return String.Empty
+        Else
+            Return path
+        End If
+
+    End Function
+
+    Private Function getCookie_Chrome() As String
+        Dim strPath As String = ""
+        Dim strDb As String = ""
+        strPath = GetChromeCookiePath()
+        Dim strReturn As String = ""
+
+        If strPath = String.Empty Then
+            Return ""
+        End If
+
+        Dim strTemp As String = strPath + ".temp"
+        If System.IO.File.Exists(strTemp) Then System.IO.File.Delete(strTemp)
+        System.IO.File.Copy(strPath, strTemp)
+        Try
+
+            strDb = String.Format("Data source={0};Version\=3;Pooling=true", strTemp)
+            'strDb = "Data source=:memory:;Version=3;Pooling=true;"
+            Dim sqlcon As New SQLiteConnection(strDb)
+            Dim cmd As SQLiteCommand = sqlcon.CreateCommand()
+            cmd.CommandText = "SELECT name,encrypted_value FROM cookies WHERE host_key LIKE '%playbyweb.com%';"
+            sqlcon.Open()
+            Dim reader As SQLiteDataReader = cmd.ExecuteReader()
+            While reader.Read()
+                strReturn = reader.GetString(1)
+                If Not strReturn.Equals(String.Empty) Then
+                    Dim encryptedvalue() As Byte = System.Text.Encoding.ASCII.GetBytes(strReturn)
+
+                    Dim decryptedValue() As Byte = ProtectedData.Unprotect(encryptedvalue, Nothing, DataProtectionScope.CurrentUser)
+                    strReturn = System.Text.Encoding.ASCII.GetString(decryptedValue)
+                End If
+            End While
+            sqlcon.Close()
+            If System.IO.File.Exists(strTemp) Then System.IO.File.Delete(strTemp)
+            Return strReturn
+
+        Catch ex As Exception
+            Return String.Empty
+        End Try
+
+
+    End Function
+
+    Private Function GetChromeCookiePath() As String
+        Dim path As String = ""
+        path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+        path += "\Google\Chrome\User Data\Default\cookies"
+
+        If Not System.IO.File.Exists(path) Then
+            Return String.Empty
+        Else
+            Return path
+        End If
+    End Function
+End Class
